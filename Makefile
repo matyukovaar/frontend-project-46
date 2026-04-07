ed952ec76c3c3eb40f130e0eaf3743915e3f0f14
matyukovaar <matyukova.arina27@gmail.com>
2026-04-07 21:23:23 +1000

    setup enviroment and linting

---
+++ Makefile
@@ -0,0 +1,14 @@
+install:
+	npm ci
+
+gendiff:
+	node bin/gendiff.js -h
+
+lint:
+	npx eslint .
+
+publish:
+	npm publish --dry-run
+
+.PHONY: test
+
---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default [
+  js.configs.recommended,
+  stylistic.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+]
