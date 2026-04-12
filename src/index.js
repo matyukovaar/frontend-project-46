c6d10e9d76326cda4a95d10873f22388444cb2f0
matyukovaar <matyukova.arina27@gmail.com>
2026-04-12 14:32:34 +1000

    add add genDiff function for flat JSON files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { parseFile } from './parsers.js'
+import _ from 'lodash'
+
+export function genDiff(filepath1, filepath2) {
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
+
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
+  const lines = keys.map((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return `- ${key}: ${data1[key]}`
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return `+ ${key}: ${data2[key]}`
+    }
+    if (data1[key] !== data2[key]) {
+      return `- ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`
+    }
+    return `  ${key}: ${data1[key]}`
+  })
+
+  return `{\n  ${lines.join('\n  ')}\n}`
+}
